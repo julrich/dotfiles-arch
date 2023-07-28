98f433dfbe4daa50d5b2eb1765f3a8bcb91c6eda
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2023-07-28 17:47:55 +0200

    feature: add more X config, updates to picom

---
+++ home/.screenlayout/oberkassel.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1.1 --primary --mode 3840x2160 --pos 1200x0 --rotate normal --output DP-1.2 --mode 1920x1200 --pos 0x240 --rotate right --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 5040x1080 --rotate normal --output DP-2 --mode 1920x1080 --pos 5040x1080 --rotate normal --output DP-3 --off --output DP-4 --off
